8408ff6a991f8e41363953f1d97d021c5a45a365
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-07-19 20:36:42 +0900

    Add sorting by key to admin

--- simplestats/admin.py
+++ simplestats/admin.py
@@ -4,5 +4,6 @@ from simplestats.models import Stat
 
 class StatAdmin(admin.ModelAdmin):
     list_display = ('created', 'key', 'value')
+    list_filter = ('key',)
 
 admin.site.register(Stat, StatAdmin)
