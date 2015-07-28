fab8767a159e5ffb8fe912a9c645babf079fdd4d
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-07-28 18:21:30 +0900

    Add test date "diff" function

--- simplestats/admin.py
+++ simplestats/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from simplestats.models import Location, Stat
-
+from django.contrib import messages
 
 class StatAdmin(admin.ModelAdmin):
     list_display = ('created', 'key', 'value')
@@ -14,6 +14,15 @@ class LocationAdmin(admin.ModelAdmin):
     _location_url.short_description = 'Location'
     _location_url.allow_tags = True
 
+    def calculate_diff(self, request, queryset):
+        try:
+            diff = queryset[0].created - queryset[1].created
+            self.message_user(request, "Time delta is %s." % diff)
+        except:
+            self.message_user(request, "Error calculating difference. len(%d)" % len(queryset), messages.ERROR)
+    calculate_diff.short_description = 'Calculate difference between two times'
+
+    actions = ['calculate_diff']
     list_display = ('label', 'created', 'state', '_location_url')
     list_filter = ('label', 'state')
     date_hierarchy = 'created'
