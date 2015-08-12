6701bc5cdf97cb8433e12a8aea89dfb94c4f368a
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-12 22:00:20 +0900

    Move the datatable view to be part of the ViewSet

--- simplestats/urls.py
+++ simplestats/urls.py
@@ -4,5 +4,4 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     '',
     url(r'^location$', simplestats.views.LocationPattern.as_view(), name='location'),
-    url(r'^datatable$', simplestats.views.LocationDataTable.as_view(), name='locationdatatable'),
 )
--- simplestats/views.py
+++ simplestats/views.py
@@ -1,53 +1,13 @@
-import json
 import datetime
+import json
 
 from django.http import HttpResponse
 from django.views.generic.base import View
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import list_route
 
 from simplestats.models import Location
 from simplestats.serializers import LocationSerializer
-
-
-class LocationDataTable(View):
-    def dateformat(self, date):
-        return "Date(%d,%d,%d,%d,%d,%d)" % (
-            date.year, date.month - 1, date.day, date.hour, date.minute, date.second)
-
-    def get(self, request):
-        dataset = {'cols': [
-            {'id': 'Location', 'label': 'Location', 'type': 'string'},
-            {'id': 'Duration', 'label': 'Duration', 'type': 'string'},
-            {'id': 'Enter', 'pattern': 'yyyy/MM/dd H:mm:ss', 'type': 'datetime'},
-            {'id': 'Exit', 'pattern': 'yyyy/MM/dd H:mm:ss', 'type': 'datetime'},
-            {"id": "", "label": "", "pattern": "", "type": "string", "p": {"role": "tooltip"}},
-        ], 'rows': []}
-        # Later try to get proper tooltips working
-        # http://stackoverflow.com/a/11181882/622650
-
-        locations = {}
-        for location in Location.objects.filter(created__gte=datetime.datetime.now() - datetime.timedelta(days=7)):
-            if location.state == 'entered':
-                locations[location.label] = location.created
-            elif location.state == 'exited':
-                if location.label in locations:
-                    entered = locations.pop(location.label)
-                    dataset['rows'].append({'c': [
-                        {"v": location.label},
-                        {"v": str(location.created - entered)},
-                        {"v": self.dateformat(entered)},
-                        {"v": self.dateformat(location.created)},
-                        {"v": str(location.created - entered)},
-                    ]})
-
-        response = HttpResponse(content_type='application/json')
-        response.write('google.visualization.Query.setResponse(' + json.dumps({
-            'version': '0.6',
-            'table': dataset,
-            'reqId': '0',
-            'status': 'ok',
-        }) + ');')
-        return response
 
 
 class LocationPattern(View):
@@ -73,5 +33,41 @@ class LocationViewSet(
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
 
-    # def perform_create(self, serializer):
-    #     serializer.save(owner=self.request.user)
+    @list_route()
+    def datatable(self, request):
+        def dateformat(date):
+            return "Date(%d,%d,%d,%d,%d,%d)" % (
+                date.year, date.month - 1, date.day, date.hour, date.minute, date.second)
+        dataset = {'cols': [
+            {'id': 'Location', 'label': 'Location', 'type': 'string'},
+            {'id': 'Duration', 'label': 'Duration', 'type': 'string'},
+            {'id': 'Enter', 'pattern': 'yyyy/MM/dd H:mm:ss', 'type': 'datetime'},
+            {'id': 'Exit', 'pattern': 'yyyy/MM/dd H:mm:ss', 'type': 'datetime'},
+            {"id": "", "label": "", "pattern": "", "type": "string", "p": {"role": "tooltip"}},
+        ], 'rows': []}
+        # Later try to get proper tooltips working
+        # http://stackoverflow.com/a/11181882/622650
+
+        locations = {}
+        for location in Location.objects.filter(created__gte=datetime.datetime.now() - datetime.timedelta(days=7)):
+            if location.state == 'entered':
+                locations[location.label] = location.created
+            elif location.state == 'exited':
+                if location.label in locations:
+                    entered = locations.pop(location.label)
+                    dataset['rows'].append({'c': [
+                        {"v": location.label},
+                        {"v": str(location.created - entered)},
+                        {"v": dateformat(entered)},
+                        {"v": dateformat(location.created)},
+                        {"v": str(location.created - entered)},
+                    ]})
+
+        response = HttpResponse(content_type='application/json')
+        response.write('google.visualization.Query.setResponse(' + json.dumps({
+            'version': '0.6',
+            'table': dataset,
+            'reqId': '0',
+            'status': 'ok',
+        }) + ');')
+        return response
