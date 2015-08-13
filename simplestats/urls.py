69d658a1fd0540394c49436271cd530eeebef38a
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-08-13 10:30:04 +0900

    Initial timeline view

--- simplestats/urls.py
+++ simplestats/urls.py
@@ -1,7 +1,10 @@
-import simplestats.views
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+import simplestats.views
 
 urlpatterns = patterns(
     '',
     url(r'^location$', simplestats.views.LocationPattern.as_view(), name='location'),
+    url(r'^timeline', TemplateView.as_view(template_name="location_timeline.html")),
 )
