3b61c2547616ab269a747b886f2be9f72f793b61
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-09-01 20:51:11 +0900

    Use a single instance of 'now' to avoid some millisecond differences

--- simplestats/plugins/wanikani.py
+++ simplestats/plugins/wanikani.py
@@ -18,11 +18,12 @@ class WaniKani(object):
         return ''
 
     def collect(self):
+        now = datetime.datetime.utcnow()
         URL = 'https://www.wanikani.com/api/user/{}/study-queue'.format(self.api_key)
         result = requests.get(URL)
         json = result.json()
         user = json['user_information']
         info = json['requested_information']
-        yield datetime.datetime.utcnow(), 'wanikani.reviews', info['reviews_available']
-        yield datetime.datetime.utcnow(), 'wanikani.lessons', info['lessons_available']
-        yield datetime.datetime.utcnow(), 'wanikani.level', user['level']
+        yield now, 'wanikani.reviews', info['reviews_available']
+        yield now, 'wanikani.lessons', info['lessons_available']
+        yield now, 'wanikani.level', user['level']
