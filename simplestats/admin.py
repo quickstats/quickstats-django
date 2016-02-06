82dbc9a42b8351bcbb046f500c68c956c0ad697b
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-02-06 13:11:52 +0900

    Add initial countdown model

--- simplestats/admin.py
+++ simplestats/admin.py
@@ -1,11 +1,13 @@
+import simplestats.models
+import simplestats.signals
+
 from django.contrib import admin
 
-import simplestats.signals
-from simplestats.models import Stat
 
 class StatAdmin(admin.ModelAdmin):
     list_display = ('created', 'key', 'value')
     list_filter = ('key',)
     date_hierarchy = 'created'
 
-admin.site.register(Stat, StatAdmin)
+admin.site.register(simplestats.models.Stat, StatAdmin)
+admin.site.register(simplestats.models.Countdown)
--- simplestats/models.py
+++ simplestats/models.py
@@ -5,3 +5,7 @@ class Stat(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     key = models.TextField()
     value = models.FloatField()
+
+class Countdown(models.Model):
+    created = models.DateTimeField()
+    key = models.CharField(max_length=36)
