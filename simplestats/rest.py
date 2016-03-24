ec78317f194eec82d3b7d2ab7497ed35c2a384ff
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-03-24 15:51:08 +0900

    Allow ordering results

--- simplestats/rest.py
+++ simplestats/rest.py
@@ -1,18 +1,18 @@
-#from pomodoro.permissions import IsOwner
 from rest_framework import permissions, status, viewsets
 from rest_framework.authentication import (BasicAuthentication,
                                            SessionAuthentication,
                                            TokenAuthentication)
+from rest_framework.filters import OrderingFilter
 
 from simplestats.models import Chart, Countdown
 from simplestats.serializers import ChartSerializer, CountdownSerializer
 
 
 class CountdownViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    filter_backends = (OrderingFilter,)
     queryset = Countdown.objects.all()
     serializer_class = CountdownSerializer
-    #permission_classes = (IsOwner,)
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -22,9 +22,10 @@ class CountdownViewSet(viewsets.ModelViewSet):
 
 
 class ChartViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+    filter_backends = (OrderingFilter,)
     queryset = Chart.objects.all()
     serializer_class = ChartSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
