603446168e3c1b2cc996a748ace19022aeeda955
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-03-27 12:22:19 +0900

    Wrap Numerous posts in try/except for debugging

--- simplestats/numerous.py
+++ simplestats/numerous.py
@@ -1,19 +1,31 @@
+import logging
 import os
+
 import requests
+
+logger = logging.getLogger(__name__)
 
 class Numerous(object):
     @classmethod
     def update_value(cls, chart, value):
-        return requests.post(
-            'https://api.numerousapp.com/v2/metrics/{}/events'.format(chart),
-            auth=(os.environ.get('NUMEROUS_KEY'), ''),
-            json={'value': value}
-        )
+        try:
+            return requests.post(
+                'https://api.numerousapp.com/v2/metrics/{}/events'.format(chart),
+                auth=(os.environ.get('NUMEROUS_KEY'), ''),
+                json={'value': value}
+            )
+        except IOError as e:
+            logger.warning('Error posting to Numerous: %s', e)
+            return None
 
     @classmethod
     def update_chart(cls, chart, properties):
-        return requests.put(
-            'https://api.numerousapp.com/v2/metrics/{}'.format(chart),
-            auth=(os.environ.get('NUMEROUS_KEY'), ''),
-            json=properties,
-        )
+        try:
+            return requests.put(
+                'https://api.numerousapp.com/v2/metrics/{}'.format(chart),
+                auth=(os.environ.get('NUMEROUS_KEY'), ''),
+                json=properties,
+            )
+        except IOError as e:
+            logger.warning('Error posting to Numerous: %s', e)
+            return None
