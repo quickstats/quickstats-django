7c63c7bc1e9f6c4fbe616ad089250d8d472da45b
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-03-27 12:16:56 +0900

    Fix logging instances to use correct logger

--- simplestats/signals/__init__.py
+++ simplestats/signals/__init__.py
@@ -2,11 +2,11 @@ import logging
 
 from pkg_resources import working_set
 
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
 
 for entry in working_set.iter_entry_points('simplestats.signals'):
     try:
         module = entry.load()
-        logging.debug('Loaded %s', module)
+        logger.debug('Loaded %s', module)
     except ImportError:
-        logging.warning('Error loading %s', entry)
+        logger.warning('Error loading %s', entry)
