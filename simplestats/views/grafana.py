40af439212ff61583a81195f2b7ce271fbf865b9
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-06-23 22:12:39 +0900

    First draft of querying from the database

--- simplestats/views/grafana.py
+++ simplestats/views/grafana.py
@@ -3,7 +3,12 @@ Data source for grafana
 
 https://grafana.net/plugins/grafana-simple-json-datasource
 '''
-from django.http import HttpResponse
+import json
+import time
+
+import simplestats.models as models
+
+from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
@@ -17,10 +22,26 @@ class Index(View):
 @method_decorator(csrf_exempt, name='dispatch')
 class Query(View):
     def post(self, request):
-        return HttpResponse('OK')
+        body = json.loads(request.body.decode("utf-8"))
+        #logging.getLogger('django.db.backends').setLevel(logging.DEBUG)
+
+        results = []
+        for target in body['targets']:
+            response = {
+                'target': target['target'],
+                'datapoints': []
+            }
+
+            for stat in reversed(models.Stat.objects.order_by('-created').filter(key=target['target'])[:24]):
+                response['datapoints'].append([stat.value, int(time.mktime(stat.created.timetuple()))])
+
+            print(response)
+
+            results.append(response)
+        return JsonResponse(results, safe=False)
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class Search(View):
     def post(self, request):
-        return HttpResponse('OK')
+        return JsonResponse(list(models.Stat.objects.values_list('key', flat=True).distinct('key')), safe=False)
