bfde92bea065c7efd8cc59439517eaa0e1d01eeb
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-07-10 12:54:26 +0900

    Add weather for Raleigh

--- simplestats/tasks/forecast.py
+++ simplestats/tasks/forecast.py
@@ -40,3 +40,28 @@ def fukuoka():
         key='weather.fukuoka.humidity',
         value=json['currently']['humidity']
     )
+
+
+@periodic_task(run_every=crontab(minute=0))
+def raleigh():
+    now = datetime.datetime.utcnow().replace(microsecond=0, second=0)
+    url = 'https://api.forecast.io/forecast/{0}/{1},{2}?units=si'.format(
+        os.environ.get('FORECAST_IO'),
+        35.766667,
+        -78.633333
+    )
+    result = requests.get(url)
+    result.raise_for_status()
+    json = result.json()
+
+    Stat.objects.create(
+        created=now,
+        key='weather.raleigh.temperature',
+        value=json['currently']['temperature']
+    )
+
+    Stat.objects.create(
+        created=now,
+        key='weather.raleigh.humidity',
+        value=json['currently']['humidity']
+    )
