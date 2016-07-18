cc787e471b503dbc383b0f58d0de039e47e559e6
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-07-18 09:45:52 +0900

    Enable our Feedly tasks

--- simplestats/tasks/__init__.py
+++ simplestats/tasks/__init__.py
@@ -1,5 +1,6 @@
 import simplestats.tasks.countdown
 import simplestats.tasks.currency
+import simplestats.tasks.feedly
 import simplestats.tasks.forecast
 import simplestats.tasks.pocket
 import simplestats.tasks.reports
