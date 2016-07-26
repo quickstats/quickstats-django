821571443501f94ec6503ef5c8243f37ce572570
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-07-26 22:53:25 +0900

    Simple github issue count
    Closes #16

--- simplestats/tasks/__init__.py
+++ simplestats/tasks/__init__.py
@@ -2,6 +2,7 @@ import simplestats.tasks.countdown
 import simplestats.tasks.currency
 import simplestats.tasks.feedly
 import simplestats.tasks.forecast
+import simplestats.tasks.github
 import simplestats.tasks.pocket
 import simplestats.tasks.reports
 import simplestats.tasks.rescuetime
