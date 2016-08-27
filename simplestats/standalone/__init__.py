b2be1933ba48eefc315c61694d066d99b5171dd1
Paul Traylor <kungfudiscomonkey@gmail.com>
2016-08-27 11:02:03 +0900

    Move DJANGO_SETTINGS_MODULE to a single location

--- simplestats/standalone/__init__.py
+++ simplestats/standalone/__init__.py
@@ -1,6 +1,8 @@
 import os
 import envdir
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplestats.standalone.settings")
+
 CONFIG_DIR = os.path.expanduser('~/.config/simplestats')
 
 if os.path.exists(CONFIG_DIR):
--- simplestats/standalone/manage.py
+++ simplestats/standalone/manage.py
@@ -1,9 +1,7 @@
-import os
 import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplestats.standalone.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
