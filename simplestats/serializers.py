95155e4b40fc50e47be9024f0c042352cdd1e500
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-09-01 17:46:27 +0900

    Render empty 'more' value as None

--- simplestats/serializers.py
+++ simplestats/serializers.py
@@ -3,9 +3,18 @@ from rest_framework import serializers
 from simplestats import models
 
 
+# Custom seralizer field so that empty 'more' links are returned as None instead of empty string
+class CustomURLField(serializers.URLField):
+    def to_representation(self, value):
+        if not value:
+            return None
+        return value
+
+
 class WidgetSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     meta = serializers.SerializerMethodField()
+    more = CustomURLField()
 
     def get_meta(self, obj):
         return {
