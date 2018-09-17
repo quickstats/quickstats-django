df331d428782a0168e5df2fc77bddabdd1f535eb
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-09-17 16:03:02 +0900

    Refactor samples into SubView and add NoteSubView

--- simplestats/rest.py
+++ simplestats/rest.py
@@ -14,30 +14,14 @@ from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
 
-from simplestats import grafana, models
-from simplestats.serializers import SampleSerializer, WidgetSerializer
+from simplestats import grafana, models, serializers
 
 from django.shortcuts import get_object_or_404
 
 logger = logging.getLogger(__name__)
 
 
-class WidgetViewSet(grafana.GrafanaWidgetView, viewsets.ModelViewSet):
-    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication)
-    filter_backends = (OrderingFilter,)
-    permission_classes = (DjangoModelPermissions,)
-    queryset = models.Widget.objects.all()
-    serializer_class = WidgetSerializer
-    lookup_field = 'slug'
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def get_queryset(self):
-        if self.request.user.is_authenticated:
-            return models.Widget.objects.filter(owner=self.request.user).prefetch_related('meta_set')
-        return models.Widget.objects.filter(public=True).prefetch_related('meta_set')
-
+class StatsSubView(object):
     @detail_route(methods=['get', 'post'])
     def stats(self, request, slug=None):
         return getattr(self, 'stats_' + request.method)(request, slug)
@@ -48,19 +32,62 @@ class WidgetViewSet(grafana.GrafanaWidgetView, viewsets.ModelViewSet):
 
         page = self.paginate_queryset(queryset)
         if page is not None:
-            serializer = SampleSerializer(page, many=True)
+            serializer = serializers.SampleSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
 
-        serializer = SampleSerializer(queryset, many=True)
+        serializer = serializers.SampleSerializer(queryset, many=True)
         return Response(serializer.data)
 
     def stats_POST(self, request, slug):
         chart = self.get_object()
-        serializer = SampleSerializer(data=request.data)
+        serializer = serializers.SampleSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(key=chart.keys)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class NotesSubView(object):
+    @detail_route(methods=['get', 'post'])
+    def notes(self, request, slug=None):
+        return getattr(self, 'notes_' + request.method)(request, slug)
+
+    def notes_GET(self, request, slug):
+        chart = self.get_object()
+        queryset = chart.note_set.order_by('-timestamp')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = serializers.NoteSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = serializers.NoteSerialize(queryset, many=True)
+        return Response(serializer.data)
+
+    def notes_POST(self, request, slug):
+        chart = self.get_object()
+        serializer = serializers.NoteSerialize(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(key=chart.keys)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class WidgetViewSet(StatsSubView, NotesSubView, grafana.GrafanaWidgetView, viewsets.ModelViewSet):
+    authentication_classes = (BasicAuthentication, SessionAuthentication, TokenAuthentication)
+    filter_backends = (OrderingFilter,)
+    permission_classes = (DjangoModelPermissions,)
+    queryset = models.Widget.objects.all()
+    serializer_class = serializers.WidgetSerializer
+    lookup_field = 'slug'
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return models.Widget.objects.filter(owner=self.request.user).prefetch_related('meta_set')
+        return models.Widget.objects.filter(public=True).prefetch_related('meta_set')
 
     @detail_route(methods=['post'], permission_classes=[])
     def ifttt(self, request, slug=None):
