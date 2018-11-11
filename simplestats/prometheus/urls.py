c3889b9985b08150909d3ce82f7a4de68d9f2880
Paul Traylor <kungfudiscomonkey@gmail.com>
2018-11-11 16:34:12 +0900

    Minor fix to path to match pushgateway

--- simplestats/prometheus/urls.py
+++ simplestats/prometheus/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.urls import path, re_path
 
 urlpatterns = [
-    path("jobs/<api_key>", views.PushGateway.as_view(), name='pushgateway'),
+    path("metrics/job/<api_key>", views.PushGateway.as_view(), name='pushgateway'),
     path('metrics', views.Metrics.as_view()),
     re_path('metrics/job/(?P<api_key>[0-9a-fA-F]+)(/(?P<extra>.*))?', views.PushGateway.as_view()),
 ]
