0f27237f8bc5b7a53ca08e85d48af50d088b0848
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-03-20 14:48:44 +0900

    Fix sample-list link

--- quickstats/urls.py
+++ quickstats/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("subscription/<pk>/delete", views.SubscriptionDelete.as_view(), name="subscription-delete"),
     path("user/<username>/subscriptions", views.SubscriptionListView.as_view(), name="subscriptions"),
     path("user/<username>/widgets", views.UserWidgets.as_view(), name="widget-user"),
+    # Widget Views
+    path("widget", views.WidgetListView.as_view(), name="widget-list"),
     path("widget/<pk>", views.WidgetDetailView.as_view(), name="widget-detail"),
     path("widget/<pk>/comment", views.WidgetComment.as_view(), name="widget-comment"),
     path("widget/<pk>/delete", views.WidgetDelete.as_view(), name="widget-delete"),
@@ -15,10 +17,10 @@ urlpatterns = [
     path("widget/<pk>/unsubscribe", views.WidgetUnsubscribe.as_view(), name="widget-unsubscribe"),
     path("widget/<pk>/update", views.WidgetUpdate.as_view(), name="widget-update"),
     path("widget/<pk>/increment", views.StreakIncrement.as_view(), name="streak-increment"),
-    path("widget", views.WidgetListView.as_view(), name="widget-list"),
+    # Misc Views
     path("comments", views.CommentList.as_view(), name="comment-list"),
     path("waypoints", views.WaypointList.as_view(), name="waypoint-list"),
-    # Custom Views
+    # Filtered List Views
     path("charts", views.ChartList.as_view(), name="chart-list"),
     path("countdowns", views.CountdownList.as_view(), name="countdown-list"),
     path("locations", views.LocationList.as_view(), name="location-list"),
