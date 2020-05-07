3406b795e16238cc67d759c3c03fef260b5b3d66
Paul Traylor <kungfudiscomonkey@gmail.com>
2020-05-07 19:40:32 +0900

    Update paginator

--- quickstats/templatetags/quickstats.py
+++ quickstats/templatetags/quickstats.py
@@ -1,13 +1,14 @@
 from django import template
-from django.utils.safestring import mark_safe
+from django.core.paginator import EmptyPage
 from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 
 @register.filter
 def formatted(widget):
-    if widget.type == 'countdown':
+    if widget.type == "countdown":
         return mark_safe(
             "<time class='countdown' datetime='{}'>{}</time>".format(
                 widget.timestamp.isoformat(), widget.timestamp.ctime()
@@ -18,6 +19,66 @@ def formatted(widget):
 
 @register.filter
 def embedcode(widget, request):
-    return '<iframe width="250" height="250" src="%s" sandbox="allow-top-navigation" scrolling="no" frameborder="0"></iframe>' % request.build_absolute_uri(
-        reverse("api:widget-embed", args=(widget.pk,))
+    return (
+        '<iframe width="250" height="250" src="%s" sandbox="allow-top-navigation" scrolling="no" frameborder="0"></iframe>'
+        % request.build_absolute_uri(reverse("api:widget-embed", args=(widget.pk,)))
     )
+
+
+# Taken from here
+# https://romanvm.pythonanywhere.com/post/bootstrap4-based-pagination-django-listview-30/
+
+
+@register.inclusion_tag("quickstats/paginator.html", takes_context=True)
+def render_paginator(context, adjacent_pages=3):
+    """
+    Inclusion tag
+
+    Renders paginator for multi-page lists.
+
+    Adds pagination context variables for use in displaying first, adjacent and
+    last page links in addition to those created by the object_list generic
+    view.
+
+    :param context: parent template context
+    :param adjacent_pages: the number of pages adjacent to the current
+    :return: context for rendering paginator html code
+    """
+    start_page = max(context["page_obj"].number - adjacent_pages, 1)
+    if start_page <= 3:
+        start_page = 1
+
+    end_page = context["page_obj"].number + adjacent_pages + 1
+    if end_page >= context["paginator"].num_pages - 1:
+        end_page = context["paginator"].num_pages + 1
+
+    page_numbers = [
+        n
+        for n in range(start_page, end_page)
+        if n in range(1, context["paginator"].num_pages + 1)
+    ]
+
+    try:
+        next_ = context["page_obj"].next_page_number()
+    except EmptyPage:
+        next_ = None
+
+    try:
+        previous = context["page_obj"].previous_page_number()
+    except EmptyPage:
+        previous = None
+
+    return {
+        "page_obj": context["page_obj"],
+        "paginator": context["paginator"],
+        "page": context["page_obj"].number,
+        "pages": context["paginator"].num_pages,
+        "page_numbers": page_numbers,
+        "next": next_,
+        "previous": previous,
+        "has_next": context["page_obj"].has_next(),
+        "has_previous": context["page_obj"].has_previous(),
+        "show_first": 1 not in page_numbers,
+        "show_last": context["paginator"].num_pages not in page_numbers,
+        "request": context["request"],
+    }
