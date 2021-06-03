3f5e9dc31d51af1cdbfad56fb8e7ef5dd4b16700
Paul Traylor <kungfudiscomonkey@gmail.com>
2021-06-03 19:24:12 +0900

    Move user agent bits

---
+++ quickstats/client.py
@@ -0,0 +1,15 @@
+import requests
+
+from django.conf import settings
+
+from quickstats.version import __VERSION__
+
+DEFAULT_USER_AGENT = f"django-quickstats/{__VERSION__}"
+
+USER_AGENT = getattr(settings, "USER_AGENT", DEFAULT_USER_AGENT)
+
+
+def get(url, **kwargs):
+    headers = kwargs.setdefault("headers", {})
+    headers.setdefault("user-agent", USER_AGENT)
+    return requests.get(url, **kwargs)
--- quickstats/scrape/__init__.py
+++ quickstats/scrape/__init__.py
@@ -1,17 +1,13 @@
 import requests
 
 from django import forms
-from django.conf import settings
 
+from quickstats.client import USER_AGENT
 from quickstats.models import Scrape
-from quickstats.version import __VERSION__
 
 
 class FormScrape(forms.Form):
     url = forms.URLField()
-
-
-DEFAULT_USER_AGENT = f"django-quickstats/{__VERSION__}"
 
 
 class BaseScrape:
@@ -19,9 +15,7 @@ class BaseScrape:
 
     def __init__(self):
         self.session = requests.Session()
-        self.session.headers.setdefault(
-            "user-agent", getattr(settings, "USER_AGENT", DEFAULT_USER_AGENT)
-        )
+        self.session.headers.setdefault("user-agent", USER_AGENT)
 
     def scrape(self, config: Scrape):
         raise NotImplementedError("Missing implementation")
